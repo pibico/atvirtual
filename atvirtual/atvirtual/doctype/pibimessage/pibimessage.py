0788a60f76bca279f08addfca15bba71cc139cf2
PibiCo <pibico.sl@gmail.com>
2021-03-14 21:45:19 +0100

    modificado:     pibimessage.json 	modificado:     pibimessage.py 	borrado:        ../email_recipients/__init__.py 	borrado:        ../email_recipients/email_recipients.js 	borrado:        ../email_recipients/email_recipients.json         borrado:        ../email_recipients/email_recipients.py 	borrado:        ../email_recipients/test_email_recipients.py 	modificado:     ../../../fixtures/custom_script.json 	modificado:     ../../../fixtures/property_setter.json 	modificado:     ../../../fixtures/translation.json 	../recipient_item/

--- atvirtual/atvirtual/doctype/pibimessage/pibimessage.py
+++ atvirtual/atvirtual/doctype/pibimessage/pibimessage.py
@@ -24,126 +24,144 @@ class pibiMessage(Document):
     sms_list = []
     telegram_list = []
     mqtt_list = []
-    attachment = ''
+    str_attach = ''
+    recipients = []
     
-    if self.device == '':
-      if self.participant_role != "" and self.course != "":
-        """ Get from database devices ported by user in session """
-        devices = frappe.db.sql("""SELECT device FROM `tabSession Role Item` WHERE parent=%s AND participant_role=%s and docstatus < 2""", (self.course, self.participant_role), True) 
-        for item in devices:
-          doc = frappe.get_doc('Device', item.device)
-          if not doc.disabled:
-            if doc.is_connected and not doc.is_scanner and doc.alerts_active:
-              if doc.by_sms:
-                if doc.sms_number != '':
-                  sms_list.append(doc.sms_number)
-                  #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))
-              if doc.by_text:
-                if doc.device_name != '' and doc.by_mqtt:
-                  mqtt_list.append({"name": doc.device_name})
-                  #frappe.msgprint(_("Message by mqtt to ") + str(doc.device_name))
-                elif not doc.by_mqtt:
-                  if doc.sms_number != '' and not doc.sms_number in sms_list:
-                    sms_list.append(doc.sms_number)  
-                    #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))
-              if doc.by_telegram:
-                if doc.telegram_number != '':
-                  telegram_list.append(doc.telegram_number)
-                  #frappe.msgprint(_("Message by sms to ") + str(doc.telegram_number))
-    else:
-      doc = frappe.get_doc('Device', self.device)
-      if not doc.disabled:
-        if doc.is_connected and doc.alerts_active:
-          if doc.by_sms:
-            if doc.sms_number != '':
-              if not doc.sms_number in sms_list:
-                sms_list.append(doc.sms_number)
-                #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))
-          if doc.by_text:
-            if doc.device_name != '' and doc.by_mqtt:
-              mqtt_list.append({"name": doc.device_name})
-              #frappe.msgprint(_("Message by mqtt to ") + str(doc.device_name))
-            elif not doc.by_mqtt:
-              if doc.sms_number != '' and not doc.sms_number in sms_list:
-                sms_list.append(doc.sms_number)  
-                #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))  
-          if doc.by_telegram:
-            if doc.telegram_number != '':
-              if not doc.telegram_number in telegram_list:
-                telegram_list.append(doc.telegram_number)
-                #frappe.msgprint(_("Message by sms to ") + str(doc.telegram_number))
-    
-    if len(self.message_item) > 0:  
-      for row in self.message_item:
-        if "http" in row.attachment:
-          attachment = attachment + str(row.description) + ": " + str(row.attachment) + " \n"  
-        else:
-          attachment = attachment + str(row.description) + ": " + frappe.utils.get_url() + urllib.parse.quote(row.attachment) + " \n"        
+    if self.message_type == "E-mail":
+      message = self.email_body
+      recipient_list = self.recipient_item
+      if len(recipient_list) > 0:
+        for item in recipient_list:
+          recipients.append(item.participant_email_id)
+      if len(self.message_item) > 0:
+        for idx, row in enumerate(self.message_item):
+          if "http" in row.attachment:
+            str_attach = str_attach + '<a href="' + row.attachment + '">Anexo ' +str(idx+1) + ': ' + row.description + '</a><br>'
+          else:   
+            str_attach = str_attach + '<a href="' + frappe.utils.get_url() + urllib.parse.quote(row.attachment) + '">Anexo ' +str(idx+1) + ': ' + row.description + '</a><br>'
+        message = message + "<p>Con archivos anexos:</p><p>" + str_attach + "</p>"
+      
+      ## Send message by Email
+      email_args = {
+        "sender": self.from_email_account,
+        "recipients": recipients,
+        "message": message,
+        "subject": self.subject,
+        "reference_doctype": self.doctype,
+        "reference_name": self.name
+      }
+      frappe.sendmail(**email_args)
     
     if self.message_type == "Text":
       message = self.message_text #"From AT Virtual: " + self.message_text
-      if attachment != '':
-        message = message + "\n " + attachment
+      if len(attachment) > 0:
+        message = message + "\n con archivos anexos:\n" + str(attachment)
+      
+      if self.device == '':
+        if self.participant_role != "" and self.course != "":
+          """ Get from database devices ported by user in session """
+          devices = frappe.db.sql("""SELECT device FROM `tabSession Role Item` WHERE parent=%s AND participant_role=%s and docstatus < 2""", (self.course, self.participant_role), True) 
+          for item in devices:
+            doc = frappe.get_doc('Device', item.device)
+            if not doc.disabled:
+              if doc.is_connected and not doc.is_scanner and doc.alerts_active:
+                if doc.by_sms:
+                  if doc.sms_number != '':
+                    sms_list.append(doc.sms_number)
+                    #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))
+                if doc.by_text:
+                  if doc.device_name != '' and doc.by_mqtt:
+                    mqtt_list.append({"name": doc.device_name})
+                    #frappe.msgprint(_("Message by mqtt to ") + str(doc.device_name))
+                  elif not doc.by_mqtt:
+                    if doc.sms_number != '' and not doc.sms_number in sms_list:
+                      sms_list.append(doc.sms_number)  
+                      #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))
+                if doc.by_telegram:
+                  if doc.telegram_number != '':
+                    telegram_list.append(doc.telegram_number)
+                    #frappe.msgprint(_("Message by sms to ") + str(doc.telegram_number))
+      else:
+        doc = frappe.get_doc('Device', self.device)
+        if not doc.disabled:
+          if doc.is_connected and doc.alerts_active:
+            if doc.by_sms:
+              if doc.sms_number != '':
+                if not doc.sms_number in sms_list:
+                  sms_list.append(doc.sms_number)
+                  #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))
+            if doc.by_text:
+              if doc.device_name != '' and doc.by_mqtt:
+                mqtt_list.append({"name": doc.device_name})
+                #frappe.msgprint(_("Message by mqtt to ") + str(doc.device_name))
+              elif not doc.by_mqtt:
+                if doc.sms_number != '' and not doc.sms_number in sms_list:
+                  sms_list.append(doc.sms_number)  
+                  #frappe.msgprint(_("Message by sms to ") + str(doc.sms_number))  
+            if doc.by_telegram:
+              if doc.telegram_number != '':
+                if not doc.telegram_number in telegram_list:
+                  telegram_list.append(doc.telegram_number)
+                  #frappe.msgprint(_("Message by sms to ") + str(doc.telegram_number))
     
-    ## Send message by MQTT
-    if len(mqtt_list) > 0:
-      path = frappe.utils.get_bench_path()
-      site_name = frappe.utils.get_url().replace("http://","").replace("https://","")
-      if ":" in site_name:
-        pos = site_name.find(":")
-        site_name = site_name[:pos]
+      ## Send message by MQTT
+      if len(mqtt_list) > 0:
+        path = frappe.utils.get_bench_path()
+        site_name = frappe.utils.get_url().replace("http://","").replace("https://","")
+        if ":" in site_name:
+          pos = site_name.find(":")
+          site_name = site_name[:pos]
 
-      client = frappe.get_doc('MQTT Settings', 'MQTT Settings')
-      server = client.broker_gateway
-      port = client.port
-      user = client.user
-      client.secret = get_decrypted_password('MQTT Settings', 'MQTT Settings', 'secret', False)
-      secret = client.secret
-      do_ssl = client.is_ssl
-      # connect to MQTT Broker to Publish Message
-      pid = os.getpid()
-      client_id = '{}:{}'.format('client', str(pid))
-      try:
-        backend = mqtt.Client(client_id=client_id, clean_session=True)
-        backend.username_pw_set(user, password=secret)
-        if do_ssl == True:
-          #ca = path + "/sites/" + frappe.get_site_path('private', 'files', client.ca)[1:]
-          ca = os.path.join(path, "sites", site_name, frappe.get_site_path('private', 'files', client.ca)[1:])
-          client_crt = os.path.join(path, "sites", site_name, frappe.get_site_path('private', 'files', client.client_crt)[1:])
-          client_key = os.path.join(path, "sites", site_name, frappe.get_site_path('private', 'files', client.client_key)[1:])
-          port_ssl = client.ssl_port
-          ## Prepare mqtt    
-          backend.tls_set(ca_certs=ca, certfile=client_crt, keyfile=client_key, cert_reqs=ssl.CERT_REQUIRED, ciphers=None)
-          backend.tls_insecure_set(False)
-          time.sleep(.5)
-          backend.connect(server, port_ssl)
-        else:  
-          backend.connect(server, port)
+        client = frappe.get_doc('MQTT Settings', 'MQTT Settings')
+        server = client.broker_gateway
+        port = client.port
+        user = client.user
+        client.secret = get_decrypted_password('MQTT Settings', 'MQTT Settings', 'secret', False)
+        secret = client.secret
+        do_ssl = client.is_ssl
+        # connect to MQTT Broker to Publish Message
+        pid = os.getpid()
+        client_id = '{}:{}'.format('client', str(pid))
+        try:
+          backend = mqtt.Client(client_id=client_id, clean_session=True)
+          backend.username_pw_set(user, password=secret)
+          if do_ssl == True:
+            #ca = path + "/sites/" + frappe.get_site_path('private', 'files', client.ca)[1:]
+            ca = os.path.join(path, "sites", site_name, frappe.get_site_path('private', 'files', client.ca)[1:])
+            client_crt = os.path.join(path, "sites", site_name, frappe.get_site_path('private', 'files', client.client_crt)[1:])
+            client_key = os.path.join(path, "sites", site_name, frappe.get_site_path('private', 'files', client.client_key)[1:])
+            port_ssl = client.ssl_port
+            ## Prepare mqtt    
+            backend.tls_set(ca_certs=ca, certfile=client_crt, keyfile=client_key, cert_reqs=ssl.CERT_REQUIRED, ciphers=None)
+            backend.tls_insecure_set(False)
+            time.sleep(.5)
+            backend.connect(server, port_ssl)
+          else:  
+            backend.connect(server, port)
 
-        payload = frappe.safe_decode(message).encode('utf-8')
-        for dev in mqtt_list:
-          mqtt_topic = dev['name'] + "/mqtt"
-          backend.publish(mqtt_topic, cstr(payload))
-        backend.disconnect()
-        #frappe.msgprint(_("Message by mqtt to: " + str(mqtt_list)))
-      except:
-        frappe.msgprint(_("Error in MQTT Broker sending to ", str(mqtt_list)))
-        #raise
-        pass
+          payload = frappe.safe_decode(message).encode('utf-8')
+          for dev in mqtt_list:
+            mqtt_topic = dev['name'] + "/mqtt"
+            backend.publish(mqtt_topic, cstr(payload))
+          backend.disconnect()
+          #frappe.msgprint(_("Message by mqtt to: " + str(mqtt_list)))
+        except:
+          frappe.msgprint(_("Error in MQTT Broker sending to ", str(mqtt_list)))
+          #raise
+          pass
     
-    ## Send message by Telegram
-    if len(telegram_list) > 0:
-      try:
-        send_telegram(telegram_list, cstr(message))
-        #frappe.msgprint(_("Message by telegram to: " + str(telegram_list) + " " + message))
-      except:
-        pass 
+      ## Send message by Telegram
+      if len(telegram_list) > 0:
+        try:
+          send_telegram(telegram_list, cstr(message))
+          #frappe.msgprint(_("Message by telegram to: " + str(telegram_list) + " " + message))
+        except:
+          pass 
     
-    ## Send message by SMS
-    if len(sms_list) > 0:
-      try:
-        send_sms(sms_list, cstr(message))
-        #frappe.msgprint(_("Message by sms to: " + str(sms_list) + " " + message))
-      except:
-        pass
-    
+      ## Send message by SMS
+      if len(sms_list) > 0  and self.message_type == "Text":
+        try:
+          send_sms(sms_list, cstr(message))
+          #frappe.msgprint(_("Message by sms to: " + str(sms_list) + " " + message))
+        except:
+          pass
