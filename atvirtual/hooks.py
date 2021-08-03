e2d4c636a745637fa9086dcf38ecbeb646ee1882
PibiCo <pibico.sl@gmail.com>
2021-08-03 18:02:30 +0200

    modificado:     atvirtual/atvirtual/custom.py 	modificado:     atvirtual/hooks.py

--- atvirtual/hooks.py
+++ atvirtual/hooks.py
@@ -100,8 +100,8 @@ scheduler_events = {
     "*/2 * * * *": [
  		  "atvirtual.atvirtual.custom.check_connected_devices"
  	  ],
-    "1-59 * * * *": [
- 		  "atvirtual.atvirtual.custom.sync_now"
+    "*/1 * * * *": [
+ 		  "atvirtual.atvirtual.custom.submit_scheduled_messages"
  	  ],
     "*/1 * * * *": [
       "atvirtual.atvirtual.custom.update_ongoing_course"
